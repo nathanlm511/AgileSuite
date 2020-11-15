d9f1e0cf8873be279a653a03a17040e623967b57
Nathan Moeliono <nathanmoeliono@gmail.com>
2020-11-15 12:03:23 -0500

    Updated file structure and added quote struct

---
+++ Agile Suite/QuoteStruct.swift
@@ -0,0 +1,14 @@
+//
+//  QuoteStruct.swift
+//  Agile Suite
+//
+//  Created by CS3714 on 11/15/20.
+//
+
+import Foundation
+
+struct Quote: Hashable {
+ 
+    var quote: String
+    var author: String
+}
