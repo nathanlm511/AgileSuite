74bb4628bb21a4f23c4d66ea4c4cb6fc7089d55f
Nathan Moeliono <nathanmoeliono@gmail.com>
2020-11-15 11:35:15 -0500

    Starter code

---
+++ Agile Suite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Agile Suite
+//
+//  Created by CS3714 on 11/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Agile Suite!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
